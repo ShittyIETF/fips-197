bd0579e81cad4667a6e710b3d3fb6dea83fdf69e
Shitty IETF <shitty.ietf@yandex.com>
2014-04-16 00:09:45 +0100

    Added test for 256-bit key expansion

--- aes128.cpp
+++ aes128.cpp
@@ -114,6 +114,23 @@ void testExpansions() {
   string expected128 = "2b7e151628aed2a6abf7158809cf4f3ca0fafe1788542cb123a339392a6c7605f2c295f27a96b9435935807a7359f67f3d80477d4716fe3e1e237e446d7a883bef44a541a8525b7fb671253bdb0bad00d4d1c6f87c839d87caf2b8bc11f915bc6d88a37a110b3efddbf98641ca0093fd4e54f70e5f5fc9f384a64fb24ea6dc4fead27321b58dbad2312bf5607f8d292fac7766f319fadc2128d12941575c006ed014f9a8c9ee2589e13f0cc8b6630ca6";
   static char expected192[] = "8e73b0f7da0e6452c810f32b809079e562f8ead2522c6b7bfe0c91f72402f5a5ec12068e6c827f6b0e7a95b95c56fec24db7b4bd69b5411885a74796e92538fde75fad44bb095386485af05721efb14fa448f6d94d6dce24aa326360113b30e6a25e7ed583b1cf9a27f939436a94f767c0a69407d19da4e1ec1786eb6fa64971485f703222cb8755e26d135233f0b7b340beeb282f18a2596747d26b458c553ea7e1466c9411f1df821f750aad07d753ca4005388fcc5006282d166abc3ce7b5e98ba06f448c773c8ecc720401002202";
 
+
+  Byte key256[] = {
+    0x60, 0x3d, 0xeb, 0x10, 0x15, 0xca, 0x71, 0xbe, 0x2b, 0x73, 0xae, 0xf0, 0x85, 0x7d, 0x77, 0x81,
+    0x1f, 0x35, 0x2c, 0x07, 0x3b, 0x61, 0x08, 0xd7, 0x2d, 0x98, 0x10, 0xa3, 0x09, 0x14, 0xdf, 0xf4,
+  };
+  char expected256[] =
+    "603deb1015ca71be2b73aef0857d77811f352c073b6108d7"
+    "2d9810a30914dff49ba354118e6925afa51a8b5f2067fcde"
+    "a8b09c1a93d194cdbe49846eb75d5b9ad59aecb85bf3c917"
+    "fee94248de8ebe96b5a9328a2678a647983122292f6c79b3"
+    "812c81addadf48ba24360af2fab8b46498c5bfc9bebd198e"
+    "268c3ba709e0421468007bacb2df331696e939e46c518d80"
+    "c814e20476a9fb8a5025c02d59c58239de1369676ccc5a71"
+    "fa2563959674ee155886ca5d2e2f31d77e0af1fa27cf73c3"
+    "749c47ab18501ddae2757e4f7401905acafaaae3e4d59b34"
+    "9adf6acebd10190dfe4890d1e6188d0b046df344706c631e";
+
   {
     CipherKey<128> key;
     for (int c = 0; c < ::Cipher::Cipher<128>::key_length; ++c) {
@@ -144,6 +161,23 @@ void testExpansions() {
     printf("Expected: %s\n", expected192);
     printf("Actual:   %s\n", hexaString< ::Cipher::Cipher<192>::expansion_length >(
       static_cast< array<Vec4, ::Cipher::Cipher<192>::expansion_length >& >(exp)).c_str()
+    );
+  }
+  {
+    CipherKey<256> key;
+    for (int c = 0; c < ::Cipher::Cipher<256>::key_length; ++c) {
+      for (int r = 0; r < 4; ++r) {
+        key[c][r] = key256[4*c + r];
+      }
+    }
+
+    printf("Expanding key: %s\n", hexaString<8>(static_cast<array<Vec4, 8>&>(key)).c_str());
+
+    CipherKeyExpansion<256> exp = expandKey(key);
+
+    printf("Expected: %s\n", expected256);
+    printf("Actual:   %s\n", hexaString< ::Cipher::Cipher<256>::expansion_length >(
+      static_cast< array<Vec4, ::Cipher::Cipher<256>::expansion_length >& >(exp)).c_str()
     );
   }
 }
